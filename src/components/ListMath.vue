8a7f4435c17fe295a3840088d8c17bd210646d0e
JDVelasquezO <jdvela158@gmail.com>
2019-09-11 20:15:35 -0600

    Added Mathjax library for Vue

---
+++ src/components/ListMath.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-card
+    class="mx-auto"
+    width="1300"
+  >
+    <v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-title>Funciones</v-list-item-title>
+      </v-list-item>
+
+      <v-list-group
+        value="true"
+      >
+        <template v-slot:activator>
+          <v-list-item-title>Matemáticas</v-list-item-title>
+        </template>
+
+        <v-list-group
+          no-action
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Álgebra</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="(admin, i) in admins"
+            :key="i"
+            link
+            to="/about"
+          >
+            <v-list-item-title v-text="admin[0]"></v-list-item-title>
+            <v-list-item-icon>
+              <v-icon v-text="admin[1]"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group
+          sub-group
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Geometría</v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="(crud, i) in cruds"
+            :key="i"
+          >
+            <v-list-item-title v-text="crud[0]"></v-list-item-title>
+            <v-list-item-action>
+              <v-icon v-text="crud[1]"></v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      admins: [
+        ['Propiedades de los exponentes'],
+        ['Propiedades de los radicales'],
+      ],
+      cruds: [
+        ['Ángulos de un polígono'],
+        ['Área y perímetro de cuadriláteros'],
+        ['Área y perímetro de triángulos'],
+        ['Área y perímetro del círculo'],
+      ],
+    }),
+  }
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,88 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div>
+  <h1 class="ml-6 mt-3">Álgebra</h1>
+  <br>
+   <v-card
+    max-width="400"
+    class="mx-left pa-2 ml-6"
+  >
+    <v-card-title>Función exponencial</v-card-title>
+    <v-card-text>
+      <div class="text--primary">
+        Producto
+        <vue-mathjax class="headline" :formula="formula"></vue-mathjax>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-on="on"
+        >
+          Ejemplo
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Producto de potencias de igual base
+        </v-card-title>
+
+        <v-card-text>
+          <br><div class="text--primary">
+            <vue-mathjax class="headline" :formula="productoExponencial"></vue-mathjax>
+          </div>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Entiendo, gracias
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    </v-card-actions>
+  </v-card>
+</div>
 </template>
+
+<script>
+import {VueMathjax} from 'vue-mathjax'
+
+export default {
+  components: {
+    'vue-mathjax': VueMathjax
+  },
+  name: 'HelloWorld',
+  data () {
+    return {
+      // formula: '$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$',
+      dialog: false,
+      formula: '$$a^{m}a^{n} = a^{m+n}$$',
+      productoExponencial: '$$x^{3}x^{5} = x^{3+5} = x^{8}$$',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .card1 {
+    padding-right: 30px;
+  }
+</style>
